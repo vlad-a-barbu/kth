37e2a692e7b223ce1bd2ee6d60bf6e1add66cf26
Vlad Barbu <vlad.a.brb@gmail.com>
2024-01-30 20:21:11 +0200

    WIP quine helper refactor

--- quine.c
+++ quine.c
@@ -1,14 +1,7 @@
-char s[] = {
-  
-  10,
-  0
-};
-
 #include <stdio.h>
 
 int main() {
     int i;
-
     printf("char s[] = {\n");
     for(i = 0; s[i]; i++)
         printf("\t%d,\n", s[i]);
---
+++ quine_helper.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("pass path to quine - src buffer will be appended as 's[]'\n");
+        return 1;
+    }
+    
+    FILE *f;
+    errno_t err = fopen_s(&f, *(argv + 1), "rb+");
+    if (err != 0) {
+        printf("fopen errno %d\n", err);
+        return 1;
+    }
+
+    fseek(f, 0L, SEEK_END);
+    size_t fsize = ftell(f);
+    rewind(f);
+
+    char* fbuff = malloc(fsize + 1);
+    if (fbuff == NULL) {
+        printf("fbuff malloc failed\n");
+        fclose(f);
+        return 1;
+    }
+    fread(fbuff, sizeof(char), fsize, f);
+    fbuff[fsize] = '\0';
+     
+    size_t qsize = 6 * fsize; // fix this
+    char *qbuff = malloc(qsize);
+    if (qbuff == NULL) {
+        printf("qbuff malloc failed\n");
+        free(fbuff);
+        fclose(f);
+        return 1;
+    }
+
+    int length = 0;
+    size_t remaining = qsize;
+    length += snprintf(qbuff + length, remaining, "char s[] = {\n");
+    remaining = qsize - length;
+    for (size_t i = 0; i < fsize; ++i) {
+        int written = snprintf(qbuff + length, remaining, "\t%d,\n", fbuff[i]);
+        if (written < 0 || written >= remaining) {
+            printf("qbuff overflow\n");
+            fclose(f);
+            free(fbuff);
+            free(qbuff);
+            return 1;
+        }
+        length += written;
+        remaining -= written;
+    }
+    snprintf(qbuff + length, remaining, "};\n\n%s", fbuff);
+
+    rewind(f);
+    fwrite(qbuff, sizeof(char), length, f);
+
+    fclose(f);
+    free(fbuff);
+    free(qbuff);
+
+    return 0;
+}
